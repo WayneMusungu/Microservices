19a189f8306dacb235140dc42a72d174500ff4ce
wayne <waynemuyera17@gmail.com>
2024-03-25 10:05:22 +0300

    HTTP server marks

--- Introduction_1/main.go
+++ Introduction_1/main.go
@@ -11,8 +11,11 @@ import (
 )
 
 func main() {
-	l := log.New(os.Stdout, "product-api", log.SetFlags())
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 	hh := handlers.NewHello(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
 
 	http.ListenAndServe(":9090", nil)
 }
