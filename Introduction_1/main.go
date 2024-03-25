ef9ceb30bf961bc391ff52feb34f09f6251f9cba
wayne <waynemuyera17@gmail.com>
2024-03-25 09:56:54 +0300

    Creating a Handler

--- Introduction_1/main.go
+++ Introduction_1/main.go
@@ -5,23 +5,14 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/WayneMusungu/Microservices.git/handlers"
 )
 
 func main() {
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		log.Println("Hello Mars")
-		d, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			http.Error(rw, "Ooops", http.StatusBadRequest)
-			return
-		}
-
-		fmt.Fprintf(rw, "Hello %s", d)
-	})
-
-	http.HandleFunc("/goodbye", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Goodbye Mars")
-	})
+	l := log.New(os.Stdout, "product-api", log.SetFlags())
+	hh := handlers.NewHello(l)
 
 	http.ListenAndServe(":9090", nil)
 }
