61d8ab047fc01d94b70e320677d59c9961fb0b0c
wayne <waynemuyera17@gmail.com>
2024-04-02 11:19:47 +0300

    Implementing the encoder and response writer

---
+++ product-api/handlers/products..go
@@ -0,0 +1,29 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/WayneMusungu/Microservices.git/data"
+)
+
+// Product is a http.Handler
+type Products struct {
+	l *log.Logger
+}
+
+// NewProducts creates a products handler with the given logger
+func NewProducts(l *log.Logger) *Products {
+	return &Products{l}
+}
+
+// ServeHTTP is the main entry point for the handler and staisfies the http.Handler
+// interface
+func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	lp := data.GetProducts()
+	err := lp.ToJSON(rw)
+	if err != nil {
+		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
+	}
+
+}
