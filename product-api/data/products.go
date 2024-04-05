4e7597654457349f2e526ae78f66c4417a372d0d
wayne <waynemuyera17@gmail.com>
2024-04-05 10:18:04 +0300

    Update Product

--- product-api/data/products.go
+++ product-api/data/products.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"time"
 )
@@ -30,7 +31,9 @@ type Products []*Product
 // NewEncoder provides better performance than json.Unmarshal as it does not
 // have to buffer the output into an in memory slice of bytes
 // this reduces allocations and the overheads of the service
-func (p*Products) ToJSON(w io.Writer) error {
+//
+// https://golang.org/pkg/encoding/json/#NewEncoder
+func (p *Products) ToJSON(w io.Writer) error {
 	e := json.NewEncoder(w)
 	return e.Encode(p)
 }
@@ -43,6 +46,30 @@ func GetProducts() Products {
 func AddProduct(p *Product) {
 	p.ID = getNextID()
 	productList = append(productList, p)
+}
+
+func UpdateProduct(id int, p*Product) error {
+	_, pos, err := findProduct(id)
+	if err != nil {
+		return err
+	}
+
+	p.ID = id
+	productList[pos] = p
+
+	return nil
+}
+
+var ErrProductNotFound = fmt.Errorf("Product not found")
+
+func findProduct(id int) (*Product, int, error) {
+	for i, p := range productList {
+		if p.ID == id {
+			return p, i, nil
+		}
+	}
+
+	return nil, -1, ErrProductNotFound 
 }
 
 func getNextID() int {
